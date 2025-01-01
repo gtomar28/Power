9ee2d4fcddd0dba946b8bbf9dcafb5763a8261af
gtomar28 <timar.gt28@gmail.com>
2025-01-01 18:53:16 +0530

    push notification try 7

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { RouterProvider } from 'react-router-dom';
 import router from 'routes';
 import ThemeCustomization from 'themes';
 import ScrollTop from 'components/ScrollTop';
-import { useEffect } from 'react';
-import { PushNotifications } from '@capacitor/push-notifications';
-
 
 export default function App() {
 
